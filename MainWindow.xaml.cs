95bd729427bc4cbc99ec66cbdbcf13d5bde07f21
Nikola Aleksic <nial147@yahoo.com>
2022-04-22 22:16:31 +0200

    Azurirano 22.4.2022

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,9 +21,13 @@ namespace DesktopContactsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            contacts = new List<Contact>();
 
             // kada otvorimo aplikaciju zelimo da nam prikaze
             ReadDatabase();
@@ -40,12 +44,51 @@ namespace DesktopContactsApp
 
         void ReadDatabase()
         {
+            
             using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
             {
                 connection.CreateTable<Contact>();
-                var contacts = connection.Table<Contact>().ToList();
+                contacts = connection.Table<Contact>().ToList().OrderBy(c => c.Name).ToList();
                 // ToList() nam je potreban jer bez toga varijabla contacts prima query tip podatka, koji ne mozemo da pristupimo
-             
+                // OrderBy metoda po Name-u ce sloziti imena kontakta po abecedi
+                
+            }
+
+            if (contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        // Event Handeler kojim unosom u TextBox u Main Window filtriramo i prikazujemo samo kontakte koje zelimo
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            /*
+             * Ovo radi isto kao i naredba iznad
+             * 
+            var filteredList = (from c in contacts
+                                where c.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+                                orderby c.Name
+                                select c).ToList();
+            */
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+            if(selectedContact != null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+
+                ReadDatabase();
             }
         }
     }
--- README.md
+++ README.md
@@ -7,4 +7,11 @@ Ovo je samo kratki program koji od korisnika trazi unos podataka :
 i unosi ih u bazu podataka. 
 
 Sastoji se od veoma osnovnog XAML frontend-a i osnova C#
-U odnosu na prvu C# aplikaciju, ovde je i prikazan osnovni rad sa bazama podataka. S obzirom da je zamisljena kao desktop aplikacija za jednog korisnika primenjen je lokalni SQL database. Za bazu podataka koriscen je SQLite koji je ubacen kao NuGet paket. U sledecoj verziji dodace se i ListView funkcionalnost C# kako bi korisnik video koje kontakte je uneo (koji se nalaze u bazi).
+U odnosu na prvu C# aplikaciju, ovde je i prikazan osnovni rad sa bazama podataka. S obzirom da je zamisljena kao desktop aplikacija za jednog korisnika primenjen je lokalni SQL database. Za bazu podataka koriscen je SQLite koji je ubacen kao NuGet paket. U sledecoj verziji dodace se i ListView  C# kako bi korisnik video koje kontakte je uneo (oni kontakti koji se nalaze u bazi).
+
+22.4.2022
+Dodat je i ListView da se vidi koji kontakti su do sada dodati.
+Dodat je TextBox za pretragu postojecih kontakta (iznad ListView)
+Primenjen je Data Binding, tj. veza izmedju objekata u C# i XAML. 
+Koriscen je LINQ
+Dodata je opcija da se neki kontakti izmene i izbrisu iz SQL
